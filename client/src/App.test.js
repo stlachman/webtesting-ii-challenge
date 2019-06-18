4834c01ceec30dc8b52973d021350b0638ba1494
Seth Lachman <lachmanseth17@gmail.com>
2019-06-18 16:53:56 -0400

    Added more tests for hit and fouls.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -28,7 +28,7 @@ describe("Record Ball Button", () => {
 
     // click it
     fireEvent.click(button);
-    getByText(/1 Balls/i);
+    getByText(/1 Ball/i);
     // confirm that Balls is updated to 1
     fireEvent.click(button);
     getByText(/2 Balls/i);
@@ -43,18 +43,58 @@ describe("Record Ball Button", () => {
 
 describe("Record Strike Button", () => {
   it("button click updates strike count", () => {
-    // find the button
     const { getByText } = render(<App />);
     const button = getByText(/record strike/i);
 
-    // click it
     fireEvent.click(button);
-    getByText(/1 Strikes/i);
+    getByText(/1 Strike/i);
     // confirm that Strikes is updated to 1
-    fireEvent.click(button);
-    getByText(/2 Strikes/i);
 
     fireEvent.click(button);
+    getByText(/2 Strikes/i);
+    // confirm that Strikes is updated to 1
+    fireEvent.click(button);
     getByText(/0 Strikes/i);
+  });
+});
+
+describe("Record Hit Button", () => {
+  it("button click should reset ball and strike display counts", () => {
+    const { getByText } = render(<App />);
+
+    // Record Ball
+    const ballButton = getByText(/record ball/i);
+    fireEvent.click(ballButton);
+    getByText(/1 Ball/i);
+
+    // Record One Strike
+    const strikeButton = getByText(/record strike/i);
+    fireEvent.click(strikeButton);
+    getByText(/1 Strike/i);
+
+    // Reset Displays through hit
+    const hitButton = getByText(/record hit/i);
+    fireEvent.click(hitButton);
+    getByText(/0 Balls/i);
+    getByText(/0 Strikes/i);
+  });
+});
+
+describe("Record Foul Button", () => {
+  it("button click should add strike to count unless strike count is at 2", () => {
+    const { getByText } = render(<App />);
+
+    // Record One Strike
+    const strikeButton = getByText(/record strike/i);
+    fireEvent.click(strikeButton);
+    getByText(/1 Strike/i);
+
+    // Reset Strike through foul
+    const foulButton = getByText(/record foul/i);
+    fireEvent.click(foulButton);
+    getByText(/2 Strikes/i);
+
+    fireEvent.click(foulButton);
+    getByText(/2 Strikes/i);
   });
 });
--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Display = props => {
+  const ballsText = props.count.balls === 1 ? "Ball" : "Balls";
+  const strikesText = props.count.strikes === 1 ? "Strike" : "Strikes";
   return (
     <div>
       <h1>Display</h1>
       <p>
-        {props.count.balls} Balls and {props.count.strikes} Strikes
+        {props.count.balls} {ballsText} and {props.count.strikes} {strikesText}
       </p>
     </div>
   );
